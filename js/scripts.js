00bed661749c7bf7e0da5cd938882db3beacf9d6
Mikhail Shishov <contact@michaelshishov.com>
2023-07-27 08:16:23 +0200

    Validate, review popup

--- js/scripts.js
+++ js/scripts.js
@@ -113,19 +113,18 @@ document.addEventListener(
     $('.popup-with-form').magnificPopup({
       type: 'inline',
       preloader: false,
-      focus: '#name',
 
       // When elemened is focused, some mobile browsers in some cases zoom in
       // It looks not nice, so we disable it:
-      callbacks: {
-        beforeOpen: function () {
-          if ($(window).width() < 700) {
-            this.st.focus = false;
-          } else {
-            this.st.focus = '#name';
-          }
-        },
-      },
+      // callbacks: {
+      //   beforeOpen: function () {
+      //     if ($(window).width() < 700) {
+      //       this.st.focus = false;
+      //     } else {
+      //       this.st.focus = '#name';
+      //     }
+      //   },
+      // },
     });
   },
   false
